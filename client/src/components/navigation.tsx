e4990cd09d07ae152f6a6ad6c2bf75ba2a6d5d92
barou955 <baroujan2502@gmail.com>
2025-07-23 21:11:19 +0200

    Update navigation.tsx

--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -31,8 +31,8 @@ export default function Navigation() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <div className="flex-shrink-0 flex items-center">
-            <div className="w-28 h-10 rounded-lg overflow-hidden mr-3">
+           <div className="flex-shrink-0 flex items-center -ml-4">
+            <div className="w-28 h-10 rounded-lg overflow-hidden mr-2">
               <img 
                 src={labtekLogo} 
                 alt="Labtek Logo" 
